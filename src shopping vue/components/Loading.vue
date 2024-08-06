b0bfb18b8242d76e802740f809cc957fed8d5631
Rahman <rufaiabdulrahman21@gmail.com>
2024-08-06 19:34:12 +0100

    Deleted a file

--- src shopping vue/components/Loading.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<img :src="rolling" class="flex flex-row items-center justify-center" />
-</template>
-
-<script setup>
-import rolling from '../assets/Frame 5 (1).svg'
-
-
-</script>
--- src shopping vue/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-</script>
-<template>
-      <div className="mt-24 text-center">
-        <h2 className="text-blue-700 underline lg:mt-24 text-center font-bold lg:text-4xl sm:text-4xl sm:mt-24" >LETS GET TO KNOW RAHMAN'S STORE</h2><br/>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellendus consequuntur facilis exercitationem ipsam corporis quasi id adipisci hic rem nulla perspiciatis ex aliquam, nobis optio iusto laborum nemo magnam?</p>
-    </div>
-</template>
